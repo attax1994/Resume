986df7036c22b44453f5cea9617607cf0d7b9624
attax1994 <lqytyui@qq.com>
2018-03-09 22:09:25 +0800

    progressive image

--- src/progress-bar/progress-bar.js
+++ src/progress-bar/progress-bar.js
@@ -1,11 +1,12 @@
 ;
 (function () {
+  'use strict'
   const progressBody = document.querySelector('.with-progress');
   const triggerHeight = window.innerHeight;
 
   function checkProgressBody() {
     const bottom = progressBody.getBoundingClientRect().bottom;
-    
+
     if (bottom < triggerHeight && !progressBody.classList.contains('progress-active')) {
       progressBody.classList.add('progress-active');
       window.removeEventListener('scroll', checkProgressBody, false)
---
+++ src/progressive-image/progressive-image.js
@@ -0,0 +1,85 @@
+;
+(function () {
+  'use strict';
+
+  /* function initSize() {
+    var item,
+      itemInfo;
+    for (var i = 0; i < pictures.length; i++) {
+      item = pictures[i];
+      itemInfo = item.dataset;
+      item.style.height = itemInfo.height + 'px';
+      item.style.width = itemInfo.width + 'px';
+    }
+  } */
+  // 修改初始大小
+  /* window.addEventListener('load', initSize, false); */
+
+  function inView() {
+    if (pictures.length) {
+      requestAnimationFrame(function () {
+        var windowTop = window.pageYOffset,
+          windowBottom = windowTop + window.innerHeight;
+        var imageInfo,
+          pictureTop,
+          pictureBottom;
+
+        // 检查每幅图片
+        for (var i = 0; i < pictures.length; i++) {
+          imageInfo = pictures[i].getBoundingClientRect();
+          pictureTop = windowTop + imageInfo.top;
+          pictureBottom = pictureTop + imageInfo.height;
+
+          if (windowTop < pictureBottom && windowBottom > pictureTop) {
+            loadFullImage(pictures[i]);
+          }
+        }
+
+        // 更新需要恢复的照片（放止infinite load加载更多）
+        pictures = [].slice.call(document.querySelectorAll('.progressive .preview'));
+
+      });
+    }
+  }
+
+  /**
+   * 恢复指定图片
+   * @param {Element} item 
+   */
+  function loadFullImage(item) {
+    var href = item && (item.getAttribute('data-actual') || item.href);
+    if (!href) return;
+
+    // 预加载，放入缓存
+    var img = new Image();
+    img.src = href;
+    img.onload = function () {
+      // 从缓存中去读取
+      requestAnimationFrame(function () {
+        item.src = href;
+        item.classList.add('reveal');
+      });
+    }
+  }
+
+  /**
+   * 每0.5s执行一次
+   */
+  function throttledLoad() {
+    throttler = throttler || setTimeout(function () {
+      throttler = null;
+      inView();
+    }, 500)
+  }
+
+
+  var pictures = [].slice.call(document.querySelectorAll('.progressive .preview')),
+    throttler = null;
+
+  // 初始检查
+  inView();
+
+  window.addEventListener('scroll', throttledLoad, false);
+  window.addEventListener('resize', throttledLoad, false);
+
+})();
